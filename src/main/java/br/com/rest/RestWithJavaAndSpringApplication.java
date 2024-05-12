408630402738bc6ac4b0d01d505c8c4fceb14eb5
carlosismaelad <carlosismaelad@outlook.com>
2024-05-12 20:40:20 -0300

    Initial commit

---
+++ src/main/java/br/com/rest/RestWithJavaAndSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithJavaAndSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithJavaAndSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rest/RestWithJavaAndSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithJavaAndSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
